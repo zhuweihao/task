5a8eee2d0258ee892d2ffd799c1606c8c3568303
zhuweihao <2548237631@qq.com>
2023-02-22 16:18:49 +0800

    1

--- demo/src/main/java/com/zhuweihao/demo/mapper/DoAlertMapper.java
+++ demo/src/main/java/com/zhuweihao/demo/mapper/DoAlertMapper.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zhuweihao.demo.pojo.DoAlert;
 import com.zhuweihao.demo.pojo.dto.AlertCountDTO;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public interface DoAlertMapper extends BaseMapper<DoAlert> {
 
     List<AlertCountDTO> getAlertCountByMonth();
 
+    @Select("select * from do_alert WHERE date_format(create_time, '%Y-%m')=#{date} ORDER BY create_time DESC")
     Page<DoAlert> getAlertByMonthPage(Page<DoAlert> page,String date);
     Page<DoAlert> getAlertByDayPage(Page<DoAlert> page,String date);
 
